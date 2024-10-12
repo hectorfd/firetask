36ae5e760067543e13569e5631350046a4438cf4
61565698+hectorfd@users.noreply.github.com <61565698+hectorfd@users.noreply.github.com>
2024-10-12 11:18:31 -0500

    mi base

---
+++ android/app/src/main/kotlin/com/livecode/firetask/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.livecode.firetask
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,49 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+
+  CollectionReference tasksReference = FirebaseFirestore.instance.collection("tasks");
+  // colecciones de firebase aqui...
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(onPressed: (){
+              tasksReference.get().then((QuerySnapshot value){
+                // QuerySnapshot collection = value;
+                // List<QueryDocumentSnapshot> docs = collection.docs;
+                QuerySnapshot collection = value;
+                collection.docs.forEach((QueryDocumentSnapshot element){
+                  Map<String, dynamic> myMap = element.data() as Map<String, dynamic>;
+                  print(myMap["title"]);
+                });
+              });
+            },
+                child: Text("obtener data"),
+            ),
+
+            ElevatedButton(onPressed: (){
+              tasksReference.add({
+                "title": "dormir",
+                "description":"Para recuperar fuerzas",
+                "status": false,
+              },
+              ).then((value){
+
+              });
+            },
+                child: Text("agregar"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
